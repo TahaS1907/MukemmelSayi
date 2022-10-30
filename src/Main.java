7977391596ed94974393525a517e3e33f8f8fae3
Taha Sari <saritaha1907@gmail.comgit config --global user.email saritaha1907@gmail.comgit config --global user.email saritaha1907@gmail.com>
2022-10-31 00:10:59 +0300

    commit2-bugsFixed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner=new Scanner(System.in);
+        int number,sum=0;
+        System.out.println("Lütfen bir sayı giriniz:");
+        number= scanner.nextInt();
+        for(int i=1;i<number;i++){
+            if(number%i==0){
+                sum+=i;
+            }
+
+            }
+        if(sum==number){System.out.println(number+" "+"is a perfect number.");}
+        else System.out.println(number+" "+"isn't a perfect number.");
+        }
     }
-}
