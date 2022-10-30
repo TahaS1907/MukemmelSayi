9f7871bc94a94951f1f749de88a63c8a38d618dc
Taha Sari <saritaha1907@gmail.comgit config --global user.email saritaha1907@gmail.comgit config --global user.email saritaha1907@gmail.com>
2022-10-31 00:11:54 +0300

    commit2-bugsFixed

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
         int number,sum=0;
-        System.out.println("Lütfen bir sayı giriniz:");
+        System.out.println("Please enter a number:");
         number= scanner.nextInt();
         for(int i=1;i<number;i++){
             if(number%i==0){
